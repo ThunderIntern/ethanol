af5bd6fa3c1443d054c73d2da6419e69ea299c1a
Tio304 <53498735+Tio304@users.noreply.github.com>
2021-07-17 08:57:07 +0700

    Readme Updated

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$router->post('/login','UserController@login');
 $router->post('/register','UserController');
 $router->get('/', function () use ($router) {
     return $router->app->version();
